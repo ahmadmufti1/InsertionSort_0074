9003bf783cbceadb21aab7d371fdb6621bd84313
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-12 20:30:28 +0800

    membuat fungsi untuk dapat menginput nilai array

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -3,3 +3,31 @@ using namespace std;
 
 int arr[20];
 int n;
+
+//membuat fungsi untuk menginput data ke dalam array
+void input ()
+{
+    while (true)
+    {
+        cout << "Masukkan jumlah data pada array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+        {
+            cout << "\nArray tidak boleh lebih 20\n";
+        }
+    }
+    
+    cout << endl;
+    cout << "======================" << endl;
+    cout << "Masukkan elemen array" << endl;
+    cout << "======================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
